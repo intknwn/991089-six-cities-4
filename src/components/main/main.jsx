5b0a9893733fe8369454f64800ac4a6d2d359429
intknwn <intknwn@gmail.com>
2020-06-16 10:08:14 +0800

    Удаляет неиспользуемый пакет, рефакторит код

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -7,47 +7,6 @@ const getWidth = (width) => {
   };
 };
 
-const createPlaceCards = (names) => {
-  return names.map((name) => {
-    return (
-      <article key={name} className="cities__place-card place-card">
-        <div className="place-card__mark">
-          <span>Premium</span>
-        </div>
-        <div className="cities__image-wrapper place-card__image-wrapper">
-          <a href="#">
-            <img className="place-card__image" src="img/apartment-01.jpg" width={260} height={200} alt="Place image" />
-          </a>
-        </div>
-        <div className="place-card__info">
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">€120</b>
-              <span className="place-card__price-text">/&nbsp;night</span>
-            </div>
-            <button className="place-card__bookmark-button button" type="button">
-              <svg className="place-card__bookmark-icon" width={18} height={19}>
-                <use xlinkHref="#icon-bookmark" />
-              </svg>
-              <span className="visually-hidden">To bookmarks</span>
-            </button>
-          </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={getWidth(80)} />
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">
-            <a href="#">{name}</a>
-          </h2>
-          <p className="place-card__type">Apartment</p>
-        </div>
-      </article>
-    );
-  });
-};
-
 const Main = (props) => {
   const {propertyNames, placesCount} = props;
 
@@ -142,7 +101,44 @@ const Main = (props) => {
           */}
               </form>
               <div className="cities__places-list places__list tabs__content">
-                {createPlaceCards(propertyNames)}
+                {propertyNames.map((name) => {
+                  return (
+                    <article key={name} className="cities__place-card place-card">
+                      <div className="place-card__mark">
+                        <span>Premium</span>
+                      </div>
+                      <div className="cities__image-wrapper place-card__image-wrapper">
+                        <a href="#">
+                          <img className="place-card__image" src="img/apartment-01.jpg" width={260} height={200} alt="Place image" />
+                        </a>
+                      </div>
+                      <div className="place-card__info">
+                        <div className="place-card__price-wrapper">
+                          <div className="place-card__price">
+                            <b className="place-card__price-value">€120</b>
+                            <span className="place-card__price-text">/&nbsp;night</span>
+                          </div>
+                          <button className="place-card__bookmark-button button" type="button">
+                            <svg className="place-card__bookmark-icon" width={18} height={19}>
+                              <use xlinkHref="#icon-bookmark" />
+                            </svg>
+                            <span className="visually-hidden">To bookmarks</span>
+                          </button>
+                        </div>
+                        <div className="place-card__rating rating">
+                          <div className="place-card__stars rating__stars">
+                            <span style={getWidth(80)} />
+                            <span className="visually-hidden">Rating</span>
+                          </div>
+                        </div>
+                        <h2 className="place-card__name">
+                          <a href="#">{name}</a>
+                        </h2>
+                        <p className="place-card__type">Apartment</p>
+                      </div>
+                    </article>
+                  );
+                })}
               </div>
             </section>
             <div className="cities__right-section">
